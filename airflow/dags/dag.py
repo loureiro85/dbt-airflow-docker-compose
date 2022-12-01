826e2262fbe3150f373ece1e320366a034fe2750
Gui-laptop <loureiro85@gmail.com>
2022-12-01 17:04:45 -0300

    Add requirements.txt and update dockerfile

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,6 +1,5 @@
-from airflow import DAG, macros
+from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
 from datetime import datetime
 
 # Parse nodes
@@ -8,10 +7,12 @@ import json
 JSON_MANIFEST_DBT = '/dbt/target/manifest.json'
 PARENT_MAP = 'parent_map'
 
+
 def sanitise_node_names(value):
-        segments = value.split('.')
-        if (segments[0] == 'model'):
-                return value.split('.')[-1]
+    segments = value.split('.')
+    if (segments[0] == 'model'):
+        return value.split('.')[-1]
+
 
 def get_node_structure():
     with open(JSON_MANIFEST_DBT) as json_data:
@@ -19,18 +20,18 @@ def get_node_structure():
     ancestors_data = data[PARENT_MAP]
     tree = {}
     for node in ancestors_data:
-            ancestors = list(set(ancestors_data[node]))
-            ancestors_2 = []
-            for ancestor in ancestors:
-                if (sanitise_node_names(ancestor) is not None):
-                    ancestors_2.append(sanitise_node_names(ancestor))
-                    
-            clean_node_name = sanitise_node_names(node)
-            if (clean_node_name is not None) and (ancestors_2 is not None):
-                    tree[clean_node_name] = {}
-                    tree[clean_node_name]['ancestors'] = ancestors_2
-                    tree[clean_node_name]['tags'] = data['nodes'][node]['tags']
-    
+        ancestors = list(set(ancestors_data[node]))
+        ancestors_2 = []
+        for ancestor in ancestors:
+            if (sanitise_node_names(ancestor) is not None):
+                ancestors_2.append(sanitise_node_names(ancestor))
+
+        clean_node_name = sanitise_node_names(node)
+        if (clean_node_name is not None) and (ancestors_2 is not None):
+            tree[clean_node_name] = {}
+            tree[clean_node_name]['ancestors'] = ancestors_2
+            tree[clean_node_name]['tags'] = data['nodes'][node]['tags']
+
     return tree
 
 
@@ -50,21 +51,21 @@ daily_dag = DAG(
     '4_daily_dbt_models',
     default_args=default_args,
     description='Managing dbt data pipeline',
-    schedule_interval = '@daily',
+    schedule_interval='@daily',
 )
 
 snapshot_dag = DAG(
     '3_snapshot_dbt_models',
     default_args=default_args,
     description='Managing dbt data pipeline',
-    schedule_interval = None,
+    schedule_interval=None,
 )
 
 init_once_dag = DAG(
     '2_init_once_dbt_models',
     default_args=default_args,
     description='Managing dbt data pipeline',
-    schedule_interval = None,
+    schedule_interval=None,
 )
 
 all_operators = {}
@@ -76,19 +77,20 @@ for node in nodes:
     if ('daily' in nodes[node]['tags']):
         date_end = "{{ ds }}"
         date_start = "{{ yesterday_ds }}"
-        bsh_cmd = 'cd /dbt && dbt run --models {nodeName} --vars \'{{"start_date":"{start_date}", "end_date":"{end_date}"}}\' '.format(nodeName = node, start_date = date_start, end_date = date_end)
+        bsh_cmd = 'cd /dbt && dbt run --models {nodeName} --vars \'{{"start_date":"{start_date}", "end_date":"{end_date}"}}\' '.format(
+            nodeName=node, start_date=date_start, end_date=date_end)
         tmp_operator = BashOperator(
-            task_id= node,
+            task_id=node,
             bash_command=bsh_cmd,
             dag=daily_dag,
-            depends_on_past = True
+            depends_on_past=True
         )
         all_operators[node] = tmp_operator
 
     elif ('snapshot' in nodes[node]['tags']):
-        bsh_cmd = 'cd /dbt && dbt run --models {nodeName} '.format(nodeName = node)
+        bsh_cmd = 'cd /dbt && dbt run --models {nodeName} '.format(nodeName=node)
         tmp_operator = BashOperator(
-            task_id= node,
+            task_id=node,
             bash_command=bsh_cmd,
             dag=snapshot_dag,
         )
@@ -97,9 +99,9 @@ for node in nodes:
     elif ('init-once' in nodes[node]['tags']):
         date_end = "{{ ds }}"
         date_start = "{{ yesterday_ds }}"
-        bsh_cmd = 'cd /dbt && dbt run --models {nodeName} '.format(nodeName = node)
+        bsh_cmd = 'cd /dbt && dbt run --models {nodeName} '.format(nodeName=node)
         tmp_operator = BashOperator(
-            task_id= node,
+            task_id=node,
             bash_command=bsh_cmd,
             dag=init_once_dag,
         )
--- dockerfile
+++ dockerfile
@@ -1,11 +1,14 @@
 FROM python:3.7
-RUN pip install wtforms==2.3.3 && \
-    pip install 'apache-airflow[postgres]==1.10.14' && \
-    pip install dbt==0.15 && \
-    pip install SQLAlchemy==1.3.23
+
+RUN pip uninstall zmq && pip install zmq
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
 
 RUN mkdir /project
+
 COPY scripts_airflow/ /project/scripts/
 
 RUN chmod +x /project/scripts/init.sh
+
 ENTRYPOINT [ "/project/scripts/init.sh" ]
